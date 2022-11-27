cd7b1a979cf77ecfde0c7c629b96f20872a6b1c6
unknown <desiredshellstate@gmail.com>
2022-11-27 19:20:12 +0100

    adding context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 
 function UserResults() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+
+
 
   useEffect(() => {
     // const response run async funtion here
@@ -12,24 +16,32 @@ function UserResults() {
   }, [])
 
   const fetchUsers = async () => {
+    // const hiddenVariables = {
+    //   apikey: import.meta.env.VITE_API_KEY,
+    //   token: import.meta.env.VITE_TOKEN,
+    // }
     const response = await fetch(`https://api.github.com/users`, {
       headers: {
         Authorization: `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`
       },
     })
     const data = await response.json();
-
+    console.log(data);
     setUsers(data)
-    setLoading
+    setLoading(false)
   }
 
-
-  return (
-    <>
-      {/* Search Component */}
-      UserResults
-    </>
-  )
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserResults;
