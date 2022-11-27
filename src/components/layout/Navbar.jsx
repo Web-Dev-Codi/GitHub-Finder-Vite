55e728dfa3d78a84132c04a949c4d6ed4a3fd97b
unknown <desiredshellstate@gmail.com>
2022-11-27 02:22:19 +0100

    added footer

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,9 +13,14 @@ function Navbar({ title }) {
           </Link>
         </div>
         <div className="flex-1 px-2 mx-2">
-          <Link to='/' className="btn btn-ghost btn-sm rounder-btn">
-            Home
-          </Link>
+          <div className="flex justify-end">
+            <Link to='/' className="btn btn-ghost btn-sm rounder-btn">
+              Home
+            </Link>
+            <Link to='/about' className="btn btn-ghost btn-sm rounder-btn">
+              About
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
