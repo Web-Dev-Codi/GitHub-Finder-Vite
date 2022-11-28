197b8a9be6248ab8ae64544535948b2de48dff3c
unknown <desiredshellstate@gmail.com>
2022-11-28 23:29:48 +0100

    Added more instances for useContext

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -6,11 +6,15 @@ function UserItem({ user: { login, avatar_url } }) {
   return (
     <div className="card shadow-md compact side bg-base-100">
       <div className="flex-row items-center space-x-4 card-body">
-        <div className="avatar">
-          <div className="rounded-full shadow w-14 h-14">
-            <img src={avatar_url} alt="avatar" />
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
+            </div>
           </div>
-          <h2 className="card-title ml-2">
+        </div>
+        <div>
+          <h2 className="card-title">
             {login}
           </h2>
           <Link
