3048b1bba12601a78a7f6dda13067fce9a4b9c09
unknown <desiredshellstate@gmail.com>
2022-11-30 02:39:53 +0100

    added Alerts

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,34 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext';
 
 export default function App() {
   return (
     <GithubProvider >
-      <Router >
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <AlertProvider>
+        <Router >
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
 
-          <Footer />
-        </div>
+            </main>
 
-      </Router>
+            <Footer />
+          </div>
+
+        </Router>
+      </AlertProvider >
     </GithubProvider>
   )
 }
